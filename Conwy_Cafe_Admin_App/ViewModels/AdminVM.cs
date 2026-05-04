25714c0b8325e989782529e3d991d79328051049
Levi Varughese Mathew <levivarghesemathew@gmail.com>
2026-05-04 20:25:26 +0100

    UI
    Copied the ADMIN blueprint for BayWyn Couriers for the project
    
    Copied the style sheet as well.

---
+++ Conwy_Cafe_Admin_App/ViewModels/AdminVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conwy_Cafe_Admin_App.ViewModels
+{
+    public class AdminVM
+    {
+        public AdminVM()
+        {
+            // Null constructor
+        }
+
+    }
+}
